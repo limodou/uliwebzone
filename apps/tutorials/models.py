863a0b274bd65edccde2f14484bfd4b4077ed974
limodou <limodou@gmail.com>
2012-04-04 21:53:39 +0800

    add tutorial app

--- apps/tutorials/models.py
+++ apps/tutorials/models.py
@@ -15,7 +15,7 @@ class Tutorials(Model):
     
     title = Field(str, max_length=255, verbose_name='标题', nullable=False, required=True)
     creator = Reference('user', verbose_name='创建者', collection_name='user_tutorials')
-    authors = ManyToMany('user', verbose_name='作者', nullable=False, required=True, collection_name='authors_tutorials')
+    authors = ManyToMany('user', verbose_name='共同作者', nullable=False, collection_name='authors_tutorials')
     create_date = Field(datetime, verbose_name='创建时间', nullable=False, auto_now_add=True)
     modified_date = Field(datetime, verbose_name='修改时间', auto_now=True, auto_now_add=True)
     hits = Field(int, verbose_name='点击次数')
@@ -24,15 +24,32 @@ class Tutorials(Model):
     summary = Field(TEXT, verbose_name='介绍', default='', nullable=False)
     deleted = Field(bool, verbose_name='删除标志')
     
-class Chapters(Model):
+    def __unicode__(self):
+        return self.title
+    
+    class AddForm:
+        fields = ['title', 'authors', 'summary']
+        
+    class EditForm:
+        fields = ['title', 'authors', 'summary']
+
+    def get_image(self):
+        from uliweb import functions
+        
+        if self.image:
+            return functions.get_href(self.image)
+        else:
+            return functions.url_for_static('tutorials/default_book.png')
+    
+class Tutorials_Chapters(Model):
     __verbose_name__ = '章节'
     
-    tutorial = Reference('tutorials', verbose_name='所属教程')
+    tutorial = Reference('tutorials', verbose_name='所属教程', collection_name='turotial_chapters')
     parent = SelfReference(verbose_name='上级章节', collection_name='children_chapters')
     title = Field(str, max_length=255, verbose_name='标题', required=True)
     order = Field(int, verbose_name='顺序号')
     content = Field(TEXT, verbose_name='内容', default='', nullable=False)
-    format = Field(CHAR, max_length=1, verbose_name='格式', choices=get_var('TUTORIALS/format'))
+    format = Field(CHAR, max_length=1, verbose_name='格式', choices=get_var('TUTORIALS/format'), default='1')
     html = Field(TEXT, verbose_name='显示内容', default='', nullable=False)
     modified_user = Reference('user', verbose_name='修改人', default=get_modified_user, auto=True, auto_add=True)
     modified_date = Field(datetime, verbose_name='修改时间', auto_now=True, auto_now_add=True)
@@ -40,14 +57,23 @@ class Chapters(Model):
     votes = Field(int, verbose_name='支持票数')
     deleted = Field(bool, verbose_name='删除标志')
     
+    def __unicode__(self):
+        return self.title
+    
+    class AddForm:
+        fields = ['title', 'content']
+    
+    class EditForm:
+        fields = ['title', 'content']
+
     @classmethod
     def OnInit(cls):
-        Index('chp_indx', cls.c.tutorial, cls.c.parent, cls.c.order)
+        Index('tutchp_indx', cls.c.tutorial, cls.c.parent, cls.c.order)
 
-class Chapters_Comments(Model):
+class Tutorials_Chapters_Comments(Model):
     __verbose_name__ = '评论'
     
-    chapter = Reference('chapters', verbose_name='所属章节')
+    chapter = Reference('tutorials_chapters', verbose_name='所属章节')
     parent = SelfReference(verbose_name='上级', collection_name='children_comments')
     anchor = Field(str, max_length=40, verbose_name='定位点')
     content = Field(TEXT, verbose_name='内容')
@@ -58,5 +84,5 @@ class Chapters_Comments(Model):
     
     @classmethod
     def OnInit(cls):
-        Index('chpcmt_indx', cls.c.chapter, cls.c.parent, cls.c.anchor)
+        Index('tutchpcmt_indx', cls.c.chapter, cls.c.parent, cls.c.anchor)
     
--- apps/tutorials/views.py
+++ apps/tutorials/views.py
@@ -1,3 +1,159 @@
 #coding=utf-8
 from uliweb import expose
+from uliweb.orm import get_model
 
+@expose('/tutorial')
+class TutorialView(object):
+    def __init__(self):
+        self.model = get_model('tutorials')
+        self.model_chapters = get_model('tutorials_chapters')
+        self.model_comments = get_model('tutorials_chapters_comments')
+        
+    @expose('')
+    def index(self):
+        """
+        教程显示首页
+        """
+        from uliweb.utils.generic import ListView
+
+        condition = (self.model.c.deleted==False)
+        
+        view = ListView(self.model, condition=condition, order_by=self.model.c.modified_date.desc())
+        return {'objects':view.objects()}
+
+    def add(self):
+        """
+        添加新教程
+        """
+        from uliweb.utils.generic import AddView
+        
+        def get_url(**kwargs):
+            return url_for(TutorialView.view, **kwargs)
+        
+        def pre_save(data):
+            data['creator'] = request.user.id
+            if request.user.id not in data['authors']:
+                data['authors'].append(request.user.id)
+            
+        view = AddView(self.model, ok_url=get_url, pre_save=pre_save)
+        return view.run()
+    
+    def view(self, id):
+        """
+        查看教程
+        """
+        from uliweb.utils.generic import DetailView
+        
+        obj = self.model.get_or_notfound(int(id))
+        if obj.deleted:
+            flash('教程已经被删除！')
+            return redirect(url_for(TutorialView.index))
+        
+        view = DetailView(self.model, obj=obj)
+        return view.run()
+    
+    def edit(self, id):
+        """
+        编辑教程
+        """
+        from uliweb.utils.generic import EditView
+
+        def pre_save(obj, data):
+            if request.user.id not in data['authors']:
+                data['authors'].append(request.user.id)
+
+        obj = self.model.get_or_notfound(int(id))
+        view = EditView(self.model, ok_url=url_for(TutorialView.view, id=id), obj=obj, pre_save=pre_save)
+        return view.run()
+        
+    def delete(self, id):
+        """
+        删除教程
+        """
+        from uliweb.utils.generic import DeleteView
+        
+        class MyDelete(DeleteView):
+            def delete(self, obj):
+                obj.deleted = True
+                obj.save()
+                
+        obj = self.model.get_or_notfound(int(id))
+        view = MyDelete(self.model, ok_url=url_for(TutorialView.index), 
+            obj=obj)
+        return view.run()
+        
+    def read(self, id):
+        """
+        阅读教程
+        """
+        obj = self.model.get_or_notfound(int(id))
+        objects = list(self.model_chapters.filter(self.model_chapters.c.tutorial == obj.id).order_by(self.model_chapters.c.parent, self.model_chapters.c.order))
+        
+        def get_chapters(objects, parent=None, level=0, parent_num=''):
+            index = 1
+            for row in objects:
+                if row._parent_ == parent:
+                    cur_num = parent_num + str(index)
+                    yield level, cur_num, row
+                    for x in get_chapters(objects, row.id, level+1, cur_num+'.'):
+                        yield x
+                    index += 1
+        return {'object':obj, 'objects':get_chapters(objects)}
+    
+    def add_chapter(self, t_id):
+        """
+        増加章节, t_id为教程id
+        """
+        from uliweb.utils.generic import AddView
+        from sqlalchemy.sql import func
+        
+        obj = self.model.get_or_notfound(int(t_id))
+
+        def get_url(**kwargs):
+            return url_for(TutorialView.view_chapter, **kwargs)
+        
+        def pre_save(data):
+            if 'parent' in request.GET:
+                data['parent'] = int(request.GET.get('parent'))
+            data['tutorial'] = int(t_id)
+            order, = self.model_chapters.filter(self.model_chapters.c.tutorial==int(t_id)).values_one(func.max(self.model_chapters.c.order))
+            data['order'] = order+1 if order>0 else 1
+            
+        template_data = {'object':obj}
+        view = AddView(self.model_chapters, ok_url=get_url, pre_save=pre_save,
+            template_data=template_data)
+        return view.run()
+        
+    def view_chapter(self, id):
+        """
+        查看教程
+        """
+#        from uliweb.utils.generic import DetailView
+        from par import WikiGrammar, WikiHtmlVisitor
+        
+        obj = self.model_chapters.get_or_notfound(int(id))
+        if obj.deleted:
+            flash('此章节已经被删除！')
+            return redirect(url_for(TutorialView.read, id=obj._tutorial_))
+        
+#        view = DetailView(self.model_chapters, obj=obj)
+#        return view.run()
+
+        g = WikiGrammar()
+        result, rest = g.parse(obj.content, resultSoFar=[], skipWS=False)
+        tag_class = {
+            'table':'table',
+        }
+        content = WikiHtmlVisitor('', tag_class).visit(result)
+        return {'object':obj, 'content':content}
+    
+    def edit_chapter(self, id):
+        """
+        编辑章节
+        """
+        from uliweb.utils.generic import EditView
+    
+        obj = self.model_chapters.get_or_notfound(int(id))
+        view = EditView(self.model_chapters, ok_url=url_for(TutorialView.view_chapter, id=id), obj=obj)
+        return view.run()
+    
