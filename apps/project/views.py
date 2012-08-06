145d0cd30cec311e4af0cacef65a57f467c2e733
limodou <limodou@gmail.com>
2012-08-06 22:04:18 +0800

    当论坛使用 @用户名 时，注意用户名后面有空格，会给指定的用户发送消息

--- apps/project/views.py
+++ apps/project/views.py
@@ -21,3 +21,22 @@ def post_save(sender, instance, created, data, old_data):
 """ % (unicode(instance.modified_user), instance._chapter_, unicode(instance.chapter),
     text2html(instance.content))
     functions.send_message(request.user, authors, message, type='2')
+    
+import re
+re_at = re.compile('@\S+')
+def forumpost_post_save(sender, instance, created, data, old_data):
+    """
+    处理论坛发贴时当有@用户时发送消息进行通知
+    """
+    from uliweb.orm import get_model
+    from uliweb import request
+    
+    content = instance.content
+    User = get_model('user')
+    
+    message = u"""用户 %s 在论坛提到了你，<a href="/forum/id/%d">查看贴子</a>""" % (unicode(instance.posted_by), instance.id)
+    for x in re_at.findall(content):
+        user = User.get(User.c.username==x[1:])
+        if user and user.id != request.user.id:
+            functions.send_message(instance.posted_by, user, message, type='2')
+    
