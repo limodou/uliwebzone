581f479c4122ad8faa731c1194ab2a71398a0e6f
limodou <limodou@gmail.com>
2012-08-07 15:18:10 +0800

    fix send_message bug

--- apps/messages/__init__.py
+++ apps/messages/__init__.py
@@ -10,10 +10,15 @@ def send_message(from_, to_, message, type='3'):
     if not isinstance(to_, (tuple, list)):
         to_ = [to_]
     for x in to_:
+        if not isinstance(x, (int, long)):
+            x = x.id
+        if not isinstance(from_, (int, long)):
+            from_ = from_.id
+        
         obj = Message(type=type, message=message, sender=from_, user=x)
         obj.save()
     
-    #only user send message will create sended message
-    if type == '3':
-        obj = Message(type=type, message=message, sender=from_, user=to_, send_flag='s')
-        obj.save()
+        #only user send message will create sended message
+        if type == '3':
+            obj = Message(type=type, message=message, sender=from_, user=x, send_flag='s')
+            obj.save()
