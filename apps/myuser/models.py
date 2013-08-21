c1dcb4dd0adadcf75c5734e43066ec0498531781
limodou <limodou@gmail.com>
2013-08-21 16:53:04 +0800

    Fix myuser get_href bug

--- apps/myuser/models.py
+++ apps/myuser/models.py
@@ -83,7 +83,7 @@ class User(Model):
         from uliweb.contrib.staticfiles import url_for_static
         
         if self.image:
-            return get_href(self.image)
+            return functions.get_href(self.image)
         else:
             return functions.url_for_static('images/user%dx%d.jpg' % (50, 50))
         
